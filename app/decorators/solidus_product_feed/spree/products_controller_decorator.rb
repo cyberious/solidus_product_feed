1728935f32856b32ad133716f0b7c16daef7d068
Travis Fields <neoknght@gmail.com>
2020-05-15 15:30:50 -0700

    Nest into ClassMethods to override

--- app/decorators/solidus_product_feed/spree/products_controller_decorator.rb
+++ app/decorators/solidus_product_feed/spree/products_controller_decorator.rb
@@ -3,23 +3,25 @@
 module SolidusProductFeed
   module Spree
     module ProductsControllerDecorator
-      def self.prepended(klass)
-        klass.respond_to :rss, only: :index
-        klass.before_action :verify_requested_format!, only: :index
+      module ClassMethods
+        def self.prepended(klass)
+          klass.respond_to :rss, only: :index
+          klass.before_action :verify_requested_format!, only: :index
+        end
+
+        def index
+          load_feed_products if request.format.rss?
+          super
+        end
+
+        private
+
+        def load_feed_products
+          @feed_products = ::Spree::Product.all.map(&SolidusProductFeed.feed_product_class.method(:new))
+        end
+
+        ::Spree::ProductsController.prepend self
       end
-
-      def index
-        load_feed_products if request.format.rss?
-        super
-      end
-
-      private
-
-      def load_feed_products
-        @feed_products = ::Spree::Product.all.map(&SolidusProductFeed.feed_product_class.method(:new))
-      end
-
-      ::Spree::ProductsController.prepend self
     end
   end
 end
